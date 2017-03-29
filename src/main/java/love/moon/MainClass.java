2d7b2abd5928790276b0cc5e4d8c333945b0a0fa
lovemooner <lovemooner@yeah.net>
2017-03-29 15:14:32 +0800

    2

---
+++ src/main/java/love/moon/MainClass.java
@@ -0,0 +1,16 @@
+package love.moon;
+
+import org.apache.commons.lang.ArrayUtils;
+
+/**
+ * User: lovemooner
+ * Date: 17-3-29
+ * Time: 上午10:56
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+       String[] s={"s"};
+        System.out.println("Hi:"+ArrayUtils.isEmpty(s));
+    }
+}
